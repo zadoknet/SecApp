804ed0fe8598e9e11fb78ea51be3cc5e1408ef35
netanel zadok <zadoknet@gmail.com>
2020-01-15 22:16:15 +0200

    fragment

---
+++ app/src/main/java/zadok/jct/SecApp/RegisteredParcelsFragment.java
@@ -0,0 +1,36 @@
+package zadok.jct.SecApp;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegisteredParcelsFragment extends Fragment {
+
+    private RegisteredParcelsViewModel mViewModel;
+
+    public static RegisteredParcelsFragment newInstance() {
+        return new RegisteredParcelsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.registered_parcels_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(RegisteredParcelsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/zadok/jct/SecApp/RegisteredParcelsViewModel.java
@@ -0,0 +1,7 @@
+package zadok.jct.SecApp;
+
+import androidx.lifecycle.ViewModel;
+
+public class RegisteredParcelsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
